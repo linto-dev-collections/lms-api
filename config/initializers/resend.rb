692bec650488ef609f731b5b8a25838a7c0c9de5
NaokiYazawa <yazawa.naoki.01@gmail.com>
2026-04-09 15:27:13 +0900

    fix: ensure RESEND_API_KEY check excludes Rake tasks in production

--- config/initializers/resend.rb
+++ config/initializers/resend.rb
@@ -1,5 +1,5 @@
 Resend.api_key = ENV["RESEND_API_KEY"]
 
-if Rails.env.production? && Resend.api_key.blank?
+if Rails.env.production? && Resend.api_key.blank? && !defined?(Rake)
   raise "RESEND_API_KEY is required in production"
 end
